d0b0ab8176054f73a4b7d12ff47bf74288b7d5d7
mujahidur2004 <mujahidur2004@gmail.com>
2026-08-26 22:43:15 +0600

    solved Problem

--- B.cpp
+++ B.cpp
@@ -1,42 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
-#define ll long long
-
-int main() {
-    fast_io;
-    int t ;
+int main()
+{
+    int t;
     cin >> t;
-    while (t--) {
-        // --------- Problem Logic Starts Here ---------
-        int n,res=-1,flag=0;
+    while (t--)
+    {
+        int n;
         cin >> n;
-        vector<int> a(n+1,0);
-        for (int i = 1; i <= n; i++) cin >> a[i];
-        //sort(a.begin(),a.end());
-        for(int i=n;i>0 && flag==0;i--)
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
         {
-            for(int j=i;j>0;j--)
-            {
-                //if(a[i]==a[j])continue;
-                int k=__gcd(a[i],a[j]);
-                if(k==1)
-                {
-                    res=max(res,i+j);
-                    flag=1;
-                    break;
-                    
-                }
-            }
-            
-
+            cin >> a[i];
         }
-        cout<<res<<endl;
-        // int sum = 0;
-        // for (auto x : a) sum += x;
-        // cout << sum << endl;
-        // --------- Problem Logic Ends Here -----------
+        int ck1 = 0, ck2 = 0;
+        int cnt = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (a[i] != a[i - 1])
+                cnt++;
+        }
+        if (cnt == n)
+        {
+            cout << n << endl;
+            continue;
+        }
+        if (cnt == 1)
+        {
+            cout << 1 << endl;
+            continue;
+        }
+        if (n < 4)
+        {
+            cout << n << endl;
+            continue;
+        }
+
+        stack<int> st;
+        st.push(a[0]);
+        int f = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (st.top() != a[i])
+            {
+                st.push(a[i]);
+            }
+            else if (f && st.top() != a[i + 1] && i + 1 < n)
+            {
+                st.push(a[i + 1]);
+                st.push(a[i]);
+                i++;
+                f = 0;
+            }
+        }
+        multiset<int> st1;
+        for (int i = 0; i + 3 < n; i++)
+        {
+            st1.clear();
+            // cout<<st.size()<<" ";
+            st1.insert(a[i]);
+            st1.insert(a[i + 1]);
+            st1.insert(a[i + 2]);
+            st1.insert(a[i + 3]);
+            // cout<<st.size()<<" ";
+            if (st1.count(a[i]) == 2 || st1.count(a[i + 1]) == 2 || st1.count(a[i + 2]) == 2 || st1.count(a[i + 3]) == 2)
+            {
+                ck2 = 1;
+                break;
+            }
+        }
+
+        cout << st.size()+ck2 << endl;
     }
-    return 0;
 }
--- a.cpp
+++ a.cpp
@@ -1,27 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int t;
-    cin>>t;
-    while(t--)
-    {
-        int n,mx=0,cnt=0;
-        cin>>n;
-        vector<int>a(n);
-        for(int i=0;i<n;i++)
-        {
-            cin>>a[i];
-            if(a[i]>=mx)
-            {
-                mx=a[i];
-                cnt++;
-                a[i]=0;
-
+    cin >> t;
+    while(t--){
+        int n;
+        cin >>n;
+        int ck=1;
+        for(int i=2;i<=n;i++){
+            if((n+1) % i ==0){
+                ck=0;
+                break;
             }
-            
         }
-    
+        if(ck){
+            cout<<"YES"<<endl;
+            continue;
+        }
+        ck=1;
+        for(int i=3;i<=n+1;i++){
+            if(i%2 !=0){
+                ck=0;
+            }
+        }
+        if(ck){
+            cout<<"YES"<<endl;
+        }
+        else {
+            cout<<"NO"<<endl;
+        }
 
     }
+    
 }

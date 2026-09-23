3d492757b16d46f06f90a5efd1f5a48817ca6fd6
mujahidur2004 <mujahidur2004@gmail.com>
2026-09-23 18:48:03 +0600

    solved problem

---
+++ B_Relatively_Prime_Pairs.cpp
@@ -0,0 +1,34 @@
+/*
+BISMILLAH HIR RAHMAN NIR RAHIM
+Md Mujahidur Rahman
+Department of CSE
+Netrokona University, Bangladesh
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define ll long long
+#define nl '\n'
+#define gcd __gcd
+
+int main()
+{
+    fast_io;
+
+    ll l ,r;
+    cin >> l >> r;
+    cout<<"YES"<<nl;
+    
+    while(l<=r){
+        cout<<l <<" "<<l+1<<nl;
+        l++;
+        l++;
+
+    }
+
+    
+
+    return 0;
+}
